a6fa00895793acdf192b7d630dd13825f729a598
Luoxiaogan <1601710607@qq.com>
2023-08-27 20:36:08 +0800

    from my win

---
+++ Learn_python/Introduction/Basic_Operators.py
@@ -0,0 +1,17 @@
+remainder=11%3
+print(remainder)
+squared=3**2
+cubed=2**3
+print(squared,cubed)
+helloworld = "hello" + " " + "world"
+print(helloworld)
+lotsofhellos = "hello " * 10
+print(lotsofhellos)
+""" Lists """
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
+""" 输出三遍：[1, 2, 3, 1, 2, 3, 1, 2, 3]"""
---
+++ Practice_on_Matlab/MATLAB/Learning/nge.m
@@ -0,0 +1,5 @@
+%Program 1.5 Gaussian Elimination 
+%Naive Gaussian Elimination
+%compute apprximate solution of AX=b
+%Inpute: A, b
+%Output: X
