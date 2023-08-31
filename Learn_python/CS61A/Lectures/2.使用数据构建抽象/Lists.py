4107b907b71733030f99e3ac50c51a3ca6bc3c43
Luoxiaogan <1601710607@qq.com>
2023-08-31 12:24:12 +0800

    from my win

--- Learn_python/CS61A/Lectures/2.使用数据构建抽象/Lists.py
+++ Learn_python/CS61A/Lectures/2.使用数据构建抽象/Lists.py
@@ -32,3 +32,30 @@ def count(s,value):
         if element==value:
             total+=1
     return total
+
+pairs1=[[1,2],[2,2],[3,2],[4,4]]
+same_count=0
+for x,y in pairs1:#for every element([x,y]) in pairs1
+    if x==y:
+        same_count+=1
+print(same_count)
+
+#Ranges: A range is a sequence of consecutive(C) integers.
+range(-2,2)#-2,-1,0,1(包含-2，不包含2)
+#Not a list, it's a range
+print(len(range(-2,2)))#2-(-2)=4
+print(list(range(5,8)))
+print(list(range(4)))
+list(range(4))#Starting with 0
+
+def sum_blow(n):
+    total=0
+    for i in range(n):
+        total+=i
+    return total
+
+def cheer():
+    for _ in range(3):#don't actually care what the element is
+        print('Go Bears!')
+
+#List Comprehensions
